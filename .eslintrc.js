9bc26c27cc8e2a6ccf4af5715d92b6e08050ace3
Daniel Tieber <daniel.tieber@redbull.com>
2020-05-06 17:29:54 +0200

    chore: use eslint to lint all typescript files in ./src

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  ignorePatterns: ['**/*.test.ts', '*.js']
+}
